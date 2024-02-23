dd554e0e40a854fd0a55262e29c46c5a7337a305
Haritha-2023 <151347967+Haritha-2023@users.noreply.github.com>
2024-02-23 07:09:07 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
 terraform {
   required_version = ">= 1.1.0"
-  
+  required_providers {
+    databricks = {
+      source = "databrickslabs/databricks"
+      version = "0.6.0"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
   cloud {
     organization = "AzureDatabricks"
     workspaces {
